b8fce3a591a477ab93ac257298f30ef11b5880b7
juliusmiguelditan1 <jditan@byupathway.edu>
2025-05-19 18:56:35 +0800

    as

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,8 @@
+const full = document.querySelector("#full");
+    const today = new Date(document.lastModified);
+    full.innerHTML = "Last Modified: " + today.toLocaleString();
+
+
 // Hamburger menu
     document.getElementById('hamburger').addEventListener('click', () => {
       document.getElementById('navLinks').classList.toggle('show');
